733125cfe7f540a57d4ae754ff42a04ceb10477c
Rex Zeng <zcnrex@gmail.com>
2016-02-28 21:13:53 -0800

    crawls detailed cashback on all stores on front page

--- crawler/crawler/spiders/crawler.py
+++ crawler/crawler/spiders/crawler.py
@@ -3,6 +3,8 @@
 
 import scrapy
 import json
+import sys
+import time
 
 class Spider(scrapy.Spider):
 	name = "cashbackmonitor"
@@ -10,39 +12,65 @@ class Spider(scrapy.Spider):
 		"http://www.cashbackmonitor.com/",
 	]
 
-
+	store_title_path = '//div[@style="width:100%;text-align:center;"]/h1/span[@class="font2 s22"]/text()'
+	front_page_store_path = '//div[@class="fl c"]/table/tr'
 	def parse(self, response):
 		# item = Item()
 		stores = {}
-		for sel in response.xpath('//div[@class="fl c"]/table/tr'):
+		for sel in response.xpath(front_page_store_path):
 			# store = Store()
-			index = ''
+			rate = ''
+			name = ''
+			url = ''
 			for td in sel.xpath('td'):
-				# print td.xpath('[@class, "c"]/text()').extract()
 				class_name = str(td.xpath('@class').extract()[0])
 				if 'c' == class_name:
 					text = td.xpath('text()').extract()
-					if text != []:
-						index = str(text[0])
+					if text == []:
+						rate = str(td.xpath('a/text()').extract()[0])
+						stores[name] = {'name': name, 'url': url, 'rate': rate}
 				elif 'l tl' == class_name:
 					href = str(td.xpath('a/@href').extract()[0])
 					url = response.urljoin(href)
-					stores[index] = url
-
-		print stores
-
-		# for store in stores:
-		# 	yield scrapy.Request(store, callback = self.parse_dir_contents)
+					name = str(td.xpath('a/text()').extract()[0])
 
 
-	# def parse_dir_contents(self, response):
-	# 	for sel in response.xpath('//table/tbody/tr').extract():
-	# 		# item = Item()
-	# 		# item['name']
-	# 		print sel
-	# 		for td in sel:
-	# 			print td
-	# 			exit(0)
+		for store in stores.values():
+			yield scrapy.Request(store['url'], callback = self.parse_each_website)
+
+
+	def parse_each_website(self, response):
+		store_detail = {}
+		store_detail['name'] = str(response.xpath(self.store_title_path).extract()[0])
+		store_detail['websites'] = []
+		for sel in response.xpath('//td[@class="sp half"]/div[@class="half fl"]/table/tr'):
+			# item = Item()
+			# item['name']
+			url = ''
+			name = ''
+			rate = ''
+			website = []
+			for td in sel.xpath('td'):
+				class_name = str(td.xpath('@class').extract()[0])
+				if 'l lo' == class_name:
+					href = str(td.xpath('a/@href').extract()[0])
+					url = response.urljoin(href)
+					name = str(td.xpath('a/text()').extract()[0])
+				elif 'l' == class_name:
+					rate = td.xpath('a/text()').extract()
+					if rate == []:
+						rate = str(td.xpath('text()').extract()[0])
+						rate = rate.replace(' ', '')
+						rate = rate.replace("\r", '')
+						rate = rate.replace("\n", '')
+						rate = rate.replace("\t", '')
+					else:
+						rate = str(rate[0])
+					website.append({'name': name, 'url':url, 'rate': rate})
+			store_detail['websites'].extend(website)
+		print store_detail['name']
+		print store_detail['websites']
+		yield store_detail
 
 
 
